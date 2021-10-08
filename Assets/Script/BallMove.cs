db56739cdb67a682d0eb6c2eebad8ae8db16f32d
Lakis-NEET <nasikiqbal@gmail.com>
2021-10-08 17:07:38 +0700

    com2

---
+++ Assets/Script/BallMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    public float speed = 50.0f;
+    private Vector2 trajectoryOrigin;
+    private Rigidbody2D rigidBody2D;
+    public float xInitial;
+    public float yInitial;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D.AddForce(new Vector2(xInitial, yInitial) * speed);
+    }
+}
