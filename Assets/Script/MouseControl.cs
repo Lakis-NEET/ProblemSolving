a67d97cc261e62e691a6dbd01878a398a09bd12f
Lakis-NEET <nasikiqbal@gmail.com>
2021-10-08 18:53:08 +0700

    com5

---
+++ Assets/Script/MouseControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private Rigidbody2D rigidBody2D;
+    private Vector3 mouseClick;
+    private Camera cm;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        //mengambil camera yang saat ini digunakan
+        cm = Camera.main;
+    }
+
+    void Update()
+    {
+        //jika tombol mouse kiri diklik
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Mengambil posisi mouse ketika diklik
+            mouseClick = cm.ScreenToWorldPoint(Input.mousePosition);        
+        }
+    }
+    private void FixedUpdate()
+    {
+        //Dot akan bergerak menuju posisi mouse diklik dengan kecepatan yang sudah ditentukan
+        rigidBody2D.transform.position = Vector2.MoveTowards(transform.position, mouseClick, speed);
+    }
+
+}
