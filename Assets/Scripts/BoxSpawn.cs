fcfe034e151053f5597df64dcc3c897dd8b7825c
Lakis-NEET <nasikiqbal@gmail.com>
2021-10-09 10:30:46 +0700

    com6

---
+++ Assets/Scripts/BoxSpawn.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class BoxSpawn : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] box;
+    int ymax = 3;
+    int xmax = 6;
+
+    public void Bangkit()
+    {
+        for (int i = 0; i < Random.Range(2,5); i++)
+        {
+            int tag = Random.Range(0, 2);
+            float xPos = Random.Range(-xmax, xmax);
+            float yPos = Random.Range(-ymax, ymax);
+            Vector2 posisiRandom = new Vector2(xPos, yPos);
+            Instantiate(box[tag], posisiRandom, Quaternion.identity);
+        }
+    }
+
+    void Start()
+    {
+        Bangkit();
+    }
+}
